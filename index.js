0d989d45e9f024393e96264f80d477b9fb56d81a
Danny Peters <danny.peters97@gmail.com>
2023-11-20 16:19:19 +0100

    test

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Write your functions here, make sure to export them
 
-function double1(first) {
-  return first * 2;
-}
 
-module.exports = { double1 };
+
+const double1 = (first) => first * 2;
+
+const double2 =(first) => first*2;
+
+module.exports = { double1, double2 };
+
+
+
