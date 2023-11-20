d9e7ce67b79e316190c0e2c14c6a895fab3e7f2e
Danny Peters <danny.peters97@gmail.com>
2023-11-20 16:20:49 +0100

    blabla

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// Write your functions here, make sure to export them
+// Write your functions here, make sure to export them.
 
 
 
 const double1 = (first) => first * 2;
 
-const double2 =(first) => first*2;
+const double2 =(first) => first*2 ;
 
 module.exports = { double1, double2 };
 
